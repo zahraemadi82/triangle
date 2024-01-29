1be4ab7a7a668369971c6b44d73456bb8c92c268
User <User@DESKTOP-1364VBG>
2024-01-29 17:34:10 +0330

    Add project files.

---
+++ triangle/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter three numbers:");
+
+Console.Write("Number 1: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Number 2: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Number 3: ");
+double num3 = Convert.ToDouble(Console.ReadLine());
+
+if (num1 + num2 >num3&&num2+num3 > num1 && num1 + num3 > num2)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
